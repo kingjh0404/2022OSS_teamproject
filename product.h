74e5c60d40518b030ce9155672226d30e3bdd552
kingjh0404 <103650626+kingjh0404@users.noreply.github.com>
2022-05-14 19:34:08 +0900

    Update product.h

--- product.h
+++ product.h
@@ -2,12 +2,12 @@
 #include <string.h>
 
 typedef struct{
-	char name[60];
-	int num;
+	char name[100];
+	int num;//종류
 	int price;
 } Product;
 
 int createProduct(Product *p);
-void readProduct(Product *p); 
+void readProduct(Product p); 
 int updateProduct(Product *p);
 int deleteProduct(Product *p);  
